c5e5ac4ebaee474ffba809db8277a408bb123cbf
AshkanPourabdollah <ashkanp09@gmail.com>
2025-04-23 15:40:09 +0330

    Authentication User Completed : Signup,Login,Logout

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Project apps
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'posts_list'
+LOGOUT_REDIRECT_URL = 'posts_list'
